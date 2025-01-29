0417b462606e4f240a2ce82387f2da1a59306ae2
Fran Camacho <fcamadi@gmail.com>
2025-01-29 14:36:00 +0100

    code to plot the sales model

---
+++ Tema5_Ejercicio_plot_sales_model.R
@@ -0,0 +1,29 @@
+#install.packages("C50")  # Decision trees C5.0 algorithm
+library(C50)
+
+#install.packages("caret")  # data partitioning, confusion matrix
+library(caret)  
+
+
+sales_mean <- mean(Carseats$Sales)  # mean and median are almost the same
+Carseats$SalesFactor <- factor(ifelse(Carseats$Sales>sales_mean,"Yes","No"))
+
+CarseatsNew <- Carseats[-1]
+
+set.seed(9)
+#partitioning data frame into training (75%) and testing (25%) sets
+train_indices <- createDataPartition(CarseatsNew$SalesFactor, times=1, p=.75, list=FALSE)
+#create training set
+CarseatsNew_train <- CarseatsNew[train_indices, ]
+#create testing set
+CarseatsNew_test  <- CarseatsNew[-train_indices, ]
+#create labels sets
+CarseatsNew_train_labels <- CarseatsNew[train_indices, ]$SalesFactor
+CarseatsNew_test_labels <- CarseatsNew[-train_indices, ]$SalesFactor
+
+sales_model <- C5.0(SalesFactor ~ ., data = CarseatsNew_train)
+sales_model
+
+#plotting the model
+plot(sales_model)
+
